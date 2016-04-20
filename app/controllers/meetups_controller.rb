d02da261fd1eeaa30ec1441f3cb83e66ca4ea589
sleepydeveloper <sleepydeveloper@gmail.com>
2016-04-19 18:23:38 -0700

    meetup views

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,16 @@
 class MeetupsController < ApplicationController
+
+  before_action :set_meetup, except: [:index, :new]
+
+  def index
+    @meetups = Meetup.all
+  end
+
+  def show
+  end
+
+  private
+  def set_meetup
+    @meetup = Meetup.find params[:id]
+  end
 end
